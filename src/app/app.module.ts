27aa0132f47b6415d73578ec5069d10cfeafb88d
Savalan Suleymanli <savalan.suleymanli@gmail.com>
2022-06-24 12:36:08 +0200

    adding relevant components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,30 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
 import { AppComponent } from './app.component';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  //{ path: 'profile', component: ProfileComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
-    UserLoginFormComponent
+    UserLoginFormComponent,
+    MovieCardComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +46,8 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
     MatSnackBarModule,
     FormsModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot([])
+    MatIconModule,
+    RouterModule.forRoot(appRoutes)
    
   ],
   providers: [],
